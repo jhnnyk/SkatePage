5b1b304a45384e812d710c01c6dd78fb55d3a75b
John Knotts <jhnnyk@gmail.com>
2022-07-24 15:14:01 -0600

    page title and favicon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main>
-    <h1 class="text-3xl font-bold underline text-slate-700">
+    <h1 class="text-3xl font-bold underline text-sky-900">
       Welcome to Skate.page
     </h1>
   </main>
