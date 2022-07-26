cbee48c738c102986d47eb0a6b7a885d9cecef88
John Knotts <jhnnyk@gmail.com>
2022-07-26 09:44:45 -0600

    connect to firebase db

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import { collection, getDocs } from 'firebase/firestore';
+import db from '../firebase/config';
+
+const skateparks = ref([]);
+
+const loadSkateparks = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'skateparks'));
+    querySnapshot.forEach((doc) => {
+      skateparks.value.push({ ...doc.data(), id: doc.id });
+      // console.log(`${doc.id} => ${doc.data()}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadSkateparks();
+</script>
 
 <template>
   <main>
     <h1 class="text-3xl font-bold underline text-sky-900">
       Welcome to Skate.page
     </h1>
+    {{ skateparks }}
   </main>
 </template>
