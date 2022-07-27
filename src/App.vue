4a458d7c501a364649ef9fbb4ae98c936793ff1b
John Knotts <jhnnyk@gmail.com>
2022-07-27 08:22:33 -0600

    adding some of my own styles

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { RouterLink, RouterView } from 'vue-router';
 <template>
   <header>
     <nav>
-      <RouterLink :to="{ name: 'Home' }"> SKATE.page </RouterLink>
+      <RouterLink :to="{ name: 'Home' }" class="logo"> SKATE.page </RouterLink>
       <RouterLink :to="{ name: 'Home' }"> Home </RouterLink>
       <RouterLink :to="{ name: 'About' }"> About </RouterLink>
     </nav>
@@ -15,3 +15,33 @@ import { RouterLink, RouterView } from 'vue-router';
     <RouterView />
   </main>
 </template>
+
+<style>
+body {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  background: #245096;
+  padding: 15px 25px;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  padding-right: 1em;
+}
+
+nav a.logo {
+  font-size: 1.4em;
+  font-weight: bold;
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
