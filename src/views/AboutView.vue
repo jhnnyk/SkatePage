a090d73c54ff7dff7e4bfb6a46d932ee92ffc23c
John Knotts <jhnnyk@gmail.com>
2022-09-01 16:46:25 -0600

    mission statement

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,5 +3,13 @@
 <template>
   <main>
     <h1>About this website</h1>
+    <p>
+      This site is here to help skaters find skateparks and track which ones
+      they've visited.
+    </p>
+    <p>
+      Secondly, this site is here to help skaters track their progress with
+      journal notes and trick lists.
+    </p>
   </main>
 </template>
